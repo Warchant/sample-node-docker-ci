7707c60ffa8f5013ccd0baa401838422b174ad2c
Bogdan Vaneev <warchantua@gmail.com>
2017-04-06 00:20:47 +0300

    Test 1

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker build -t warchantua/sample-node .
+docker push warchantua/sample-node
 
-ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+ssh -i ~/.ssh/gcp-instance-1 bogdan@35.185.237.248 << EOF
+docker pull warchantua/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi warchantua/sample-node:current || true
+docker tag warchantua/sample-node:latest warchantua/sample-node:current
+docker run -d --restart always --name web -p 80:80 warchantua/sample-node:current
 EOF
